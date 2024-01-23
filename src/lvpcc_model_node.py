571ac59276157da8daea64b7ed1332c651f878a3
Kyle Walker <walker12@live.co.uk>
2024-01-23 16:42:50 +0000

    lvpcc model calcs added

--- src/lvpcc_model_node.py
+++ src/lvpcc_model_node.py
@@ -14,10 +14,14 @@ class LVPCC( Node ):
 
         super().__init__('lvpcc')
 
-        self.enc_ticks = 0
-        self.L_init = 0 # mm
-        # need to set up a service here to read initial encoder state at launch
-        self.prev_enc_state = [0, 0]
+        self.L_t = 1 # length of backbone
+        # lengths of sensors
+        self.L_init = np.array([0.5, 0.5]) # mm
+        self.L = self.L_init
+        # normalised point of sensor attachment, (0,1)
+        self.P1 = 0.3
+        self.P2 = self.P1
+
 
         self.length_sub = self.create_subscription(Float32MultiArray, '/labjack_encoder', self.update_length, 10)
         self.imu_sub = self.create_subscription(Pose, '/position_from_imu_seg1', self.update_imu, 10)
@@ -28,9 +32,33 @@ class LVPCC( Node ):
 
     def lvpcc_model(self):
 
-        # insert kinematics
-        print("")
+        M1 = self.L[0]
+        M2 = self.L[1]
+        P1 = self.P1
+        P2 = self.P2
+        L = self.L_t
+        q = self.theta
+        # offset of sensors from centre line
+        r = 0
 
+        # calculation of model kinematics
+        A1 = P2*(M1-P1*L+P1*q*r)
+        B1 = L-M2-P1*L-P2*L-q*r+M1*P2+M2*P1+P1*q*r+P2*q*r
+        Pt = A1/B1
+
+        A2 = P1*L**2 - M1*L + M1*M2 + P1*q**2*r**2 + M1*P2*L - M2*P1*L + M1*q*r
+        - P1*P2*L**2 + M2*P1*q*r - 2*P1*q*L*r + P1*P2*q*L*r
+        B2 = L*r - M2*r - q*r**2 + M1*P2*r + M2*P1*r - P1*L*r - P2*L*r + P1*q*r**2 + P2*q*r**2
+        q1 = A2/B2
+
+        A3 = -(P1*L**2 - M1*L + q**2*r**2 + M1*M2 - P2*q**2*r**2 + M1*P2*L - M2*P1*L
+                + M1*q*r + M2*q*r - q*L*r - P1*P2*L**2 - M1*P2*r*q - P1*q*L*r + P2*q*L*r + P1*P2*q*L*r)
+        B3 = L*r - M2*r - q*r**2 + M1*P2*r + M2*P1*r - P1*L*r - P2*L*r + P1*q*r**2 + P2*q*r**2
+        q2 = A3/B3
+
+        # and then calculate all forward kinematics for the two curves
+
+    # update imu reading
     def update_imu(self, data):
 
         self.euler = quaternion_to_euler(data.orientation)
@@ -38,22 +66,13 @@ class LVPCC( Node ):
         self.phi = np.rad2deg(state[0].item())
         self.theta = np.rad2deg(state[1].item())
 
+    # update length readings
     def update_length(self, data):
 
         enc_state = data.data
-        self.L = (enc_state[0]/1.98)/4 + self.L_init
+        self.L = (enc_state/1.98)/4 + self.L_init
 
         self.get_logger().info("Length: %s" % self.L)
-
-
-        # if enc_state[0] !=  self.prev_enc_state[0] or enc_state[1] !=  self.prev_enc_state[1]:
-
-        #     if enc_state[0] == 1 and enc_state[1] == 0:
-        #         self.enc_ticks += 1
-        #     elif enc_state[0] == 0 and enc_state[1] == 1:
-        #         self.enc_ticks -= 1
-
-        # self.prev_enc_state = enc_state        
 
 def main():
     rclpy.init()
